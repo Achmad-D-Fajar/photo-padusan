e9416ac58fe27235597befef9824174fca4c3add
Achmad D Fajar <achmad.dwiky.2405356@students.um.ac.id>
2026-07-03 07:47:28 +0700

    feat: remove XML declaration from SVG generation for cleaner output

--- src/lib/server/image-processing.ts
+++ src/lib/server/image-processing.ts
@@ -55,7 +55,6 @@ function buildWatermarkSvg(
   }
 
   const svg =
-    `<?xml version="1.0" encoding="UTF-8"?>` +
     `<svg xmlns="http://www.w3.org/2000/svg"` +
     ` width="${imageWidth}" height="${imageHeight}"` +
     ` viewBox="0 0 ${imageWidth} ${imageHeight}">` +
